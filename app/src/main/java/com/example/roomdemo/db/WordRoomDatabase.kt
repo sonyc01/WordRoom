cdc734c4fd6e70fc0a84a29482d6e46154b2bb7f
Matej Makara <sonyc01@gmail.com>
2021-12-02 10:05:39 +0100

    Populating the database

--- app/src/main/java/com/example/roomdemo/db/WordRoomDatabase.kt
+++ app/src/main/java/com/example/roomdemo/db/WordRoomDatabase.kt
@@ -4,11 +4,41 @@ import android.content.Context
 import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
+import androidx.sqlite.db.SupportSQLiteDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.launch
 
 @Database(entities = [Word::class], version = 1, exportSchema = false)
 abstract class WordRoomDatabase : RoomDatabase() {
 
     abstract fun wordDao(): WordDao
+
+    private class WordDatabaseCallback(
+        private val scope: CoroutineScope
+    ) : RoomDatabase.Callback() {
+
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                scope.launch {
+                    populateDatabase(database.wordDao())
+                }
+            }
+        }
+
+        suspend fun populateDatabase(wordDao: WordDao) {
+            // Delete all content here.
+            wordDao.deleteAll()
+
+            // Add sample words.
+            var word = Word("Hello")
+            wordDao.insert(word)
+            word = Word("World!")
+            wordDao.insert(word)
+            word = Word("Test")
+            wordDao.insert(word)
+        }
+    }
 
     companion object {
         // Singleton prevents multiple instances of database opening at the
@@ -16,7 +46,9 @@ abstract class WordRoomDatabase : RoomDatabase() {
         @Volatile
         private var INSTANCE: WordRoomDatabase? = null
 
-        fun getDatabase(context: Context): WordRoomDatabase {
+        fun getDatabase(context: Context,
+                        scope: CoroutineScope
+        ): WordRoomDatabase {
             // if the INSTANCE is not null, then return it,
             // if it is, then create the database
             return INSTANCE ?: synchronized(this) {
@@ -31,4 +63,6 @@ abstract class WordRoomDatabase : RoomDatabase() {
             }
         }
     }
+
+
 }
--- app/src/main/java/com/example/roomdemo/models/WordsApplication.kt
+++ app/src/main/java/com/example/roomdemo/models/WordsApplication.kt
@@ -3,8 +3,15 @@ package com.example.roomdemo.models
 import android.app.Application
 import com.example.roomdemo.db.WordRepository
 import com.example.roomdemo.db.WordRoomDatabase
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.SupervisorJob
 
 class WordsApplication: Application() {
-    val database by lazy { WordRoomDatabase.getDatabase(this) }
+    // No need to cancel this scope as it'll be torn down with the process
+    val applicationScope = CoroutineScope(SupervisorJob())
+
+    // Using by lazy so the database and the repository are only created when they're needed
+    // rather than when the application starts
+    val database by lazy { WordRoomDatabase.getDatabase(this, applicationScope) }
     val repository by lazy { WordRepository(database.wordDao()) }
 }
